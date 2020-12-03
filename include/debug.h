82bf60acd047aeecd2f2878868ecd3336d5e4518
BitBlitObviMormon <hen14020@byui.edu>
2020-12-03 14:49:04 -0800

    Divide libraries from main project - Dust off the old code too

--- include/debug.h
+++ include/debug.h
@@ -1,6 +1,8 @@
 #pragma once
 
 // Any function that has the DEPRECATED tag will be marked as deprecated and throw compiler errors.
+
+// If we're debugging then use the visual leak detector - DISABLED, not up-to-date.
 #ifdef _DEBUG
-#include "vld.h"
+// #include "vld.h"
 #endif
